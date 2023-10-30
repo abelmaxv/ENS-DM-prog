57071c00b2c353b9099b4351d87a857d21e6d010
Abel Verley <abelmaxv@gmail.com>
2023-10-30 11:50:46 +0100

    amelioration ex2

--- list.asm
+++ list.asm
@@ -7,12 +7,11 @@
 
 ; Code for question B
 LD R0, loop_count ; Deceasing counter for the upcomming for loop
-LEA R1, mem ; Stores the destination adress
-ADD R2, R1, #1 ; Stores the adress to write
+LEA R1, mem 
+ADD R1, R1, #1 ; Stores mem +1
 
-loop STR R2, R1, #0 ; Writes at adress R1 the adress R2 
+loop STR R1, R1, #-1 ; M[R1-1]<-R1 for R1 = mem + i and i = 1,...,99
 ADD R1, R1, #1 
-ADD R2, R2, #1 
 ADD R0, R0 ,#-1
 BRp loop
 
