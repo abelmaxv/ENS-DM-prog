1bcac31016e36df42569e7bb6b1a6d293adc4fb9
Abel Verley <abelmaxv@gmail.com>
2023-10-30 12:24:59 +0100

    question C exercice 2

--- list.asm
+++ list.asm
@@ -5,17 +5,31 @@
 
 .ORIG x3000
 
+main ; Test function
+
+JSR create_mem
+JSR length_mem
+BR end
+
 ; Code for question B
-LD R0, loop_count ; Deceasing counter for the upcomming for loop
+create_mem LD R0, loop_count ; Deceasing counter for the upcomming for loop
 LEA R1, mem 
 ADD R1, R1, #1 ; Stores mem +1
-
-loop STR R1, R1, #-1 ; M[R1-1]<-R1 for R1 = mem + i and i = 1,...,99
+loop1 STR R1, R1, #-1 ; M[R1-1]<-R1 for R1 = mem + i and i = 1,...,99
 ADD R1, R1, #1 
 ADD R0, R0 ,#-1
-BRp loop
+BRp loop1
+RET
 
-HALT
+; Code for question C 
+length_mem AND R0, R0, #0 ;R0<-0 (counter)
+LEA R1, mem ; R1 stores the curent cell of the list
+loop2 ADD R0, R0, #1
+LDR R1, R1, #0
+BRp loop2
+RET
+
+end HALT
 
 mem .BLKW #100 ; Memory zone for question A
 loop_count .FILL #99
